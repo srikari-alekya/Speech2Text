ec7e56a7a5c49f44bff911e4ac535ae049539ccb
P Srikari Alekya <159276024+srikari-alekya@users.noreply.github.com>
2024-10-15 23:15:20 +0530

    Update speech2text.py

--- convert speech2text/speech2text.py
+++ convert speech2text/speech2text.py
@@ -1,35 +1,4 @@
-'''import tkinter as tk
-import speech_recognition as sr
-from tkinter import messagebox
-def speech2text():
-    r=sr.Recognizer()
-    with sr.Microphone() as source:
-        try:
-            audio=r.listen(source)
-            text=r.recognize_google(audio)
-            txtSpeech.insert(tk.END,text+"\n")
 
-
-        except sr.UnknownValueError:
-            txtSpeech.insert(tk.END,"Could not understand audio\n")
-        except sr.RequestError as e:
-            txtSpeech.insert(tk.END,"Error:{0}\n".format(e))
-root=tk.Tk()
-root.title("Speech to Text")
-
-MainFrame=tk.Frame(root,bd=20 ,width =900, height=600)
-MainFrame.pack()
-
-lblTitle=tk.Label(MainFrame, font=('arial',80,'bold'),text="Speech to Text",width=18)
-lblTitle.pack()
-
-txtSpeech=tk.Text(MainFrame, font=('arial',30,'bold'),width=68,height=12)
-txtSpeech.pack()
-
-btnConvert=tk.Button(MainFrame, font=('arial',30,'bold'),text="Convert to Text",width=18,height=2,command=speech2text)
-btnConvert.pack(side=tk.LEFT,padx=5)
-
-root.mainloop()'''
 import tkinter as tk
 import speech_recognition as sr
 from tkinter import messagebox
@@ -38,7 +7,7 @@ def speech2text():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         txtSpeech.insert(tk.END, "Listening...\n")
-        root.update()  # Update the UI to show "Listening..." message
+        root.update() 
         try:
             audio = r.listen(source)
             text = r.recognize_google(audio)
